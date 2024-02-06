1fe437f59b90abc467a18888f94590f2bd16d497
h16nning <henning.flath@t-online.de>
2024-02-06 17:22:05 +0100

    Ability to create cloze cards
    - At the moment using syntax similar to anki {{c1::Cloze text goes here}} etc.
    - no UI integration
    - no option to review cards
    - #27

--- src/components/editcard/CardEditor/ClozeCardEditor.tsx
+++ src/components/editcard/CardEditor/ClozeCardEditor.tsx
@@ -2,15 +2,29 @@ import { useHotkeys } from "@mantine/hooks";
 import { RichTextEditor } from "@mantine/tiptap";
 import { IconBracketsContain } from "@tabler/icons-react";
 import { EditMode } from "../../../logic/CardTypeManager";
-import { Card, CardType } from "../../../logic/card";
+import { Card, CardType, newCards } from "../../../logic/card";
 import { Deck } from "../../../logic/deck";
 import CardEditor, { useCardEditor } from "./CardEditor";
 import classes from "./ClozeCardEditor.module.css";
 
-import { useMantineTheme } from "@mantine/core";
+import { Button, Stack, useMantineTheme } from "@mantine/core";
 import { Mark } from "@tiptap/core";
-import { mergeAttributes } from "@tiptap/react";
+import { Editor, mergeAttributes } from "@tiptap/react";
+import { useCallback, useState } from "react";
+import CardEditorFooter from "../CardEditorFooter";
+import {
+  addFailed,
+  saveFailed,
+  successfullyAdded,
+  successfullySaved,
+} from "../../custom/Notification/Notification";
+import {
+  ClozeCardUtils,
+  createClozeCardSet,
+} from "../../../logic/CardTypeImplementations/ClozeCard";
+import { useSharedValue } from "../../../logic/sharedvalue";
 
+//not used right now, use ui control or remove later
 const Gap = Mark.create({
   name: "gap",
   inline: true,
@@ -53,89 +67,95 @@ interface ClozeCardEditorProps {
   mode: EditMode;
 }
 
-/*async function finish(
-mode: EditMode,
-clear: Function,
-deck: Deck,
-card: Card<CardType.Cloze> | null,
-editor: Editor | null,
-) {
-const cardInstance = createCardInstance(card, editor);
-if (cardInstance !== null) {
-    if (mode === "edit") {
-    //SAVE
-    try {
-        console.log(cardInstance.id);
-        const numberOfUpdatedRecords = await updateCard(
-        cardInstance.id,
-        cardInstance
-        );
-        if (numberOfUpdatedRecords === 0) {
-        saveFailed();
-        return;
-        }
-        successfullySaved();
-    } catch (error) {
-        saveFailed();
-    }
-    } else {
-    //NEW
-    try {
-        await newCard(cardInstance, deck);
-        clear && clear();
-        successfullyAdded();
-    } catch (error) {
-        addFailed();
-    }
-    }
-}
-}*/
-
-/*function createCardInstance(
-card: Card<CardType.Cloze> | null,
-editor: Editor | null,
-) {
-return card
-    ? ClozeCardUtils.update(
-        {
-        frame: ClozeFrame,
-        back: backEditor?.getHTML() ?? "",
-        },
-        card
-    )
-    : ClozeCardUtils.create({
-        front: frontEditor?.getHTML() ?? "",
-        back: backEditor?.getHTML() ?? "",
-    });
-}*/
-
 export default function ClozeCardEditor({
   card,
   deck,
   mode,
 }: ClozeCardEditorProps) {
-  const theme = useMantineTheme();
-
   useHotkeys([["mod+Enter", () => {}]]);
   //fix sometime
-  const editor = useCardEditor("", Gap);
+  const editor = useCardEditor(
+    useSharedValue(card?.content.textReferenceId ?? "")?.value ?? "",
+    Gap
+  );
+
+  const clear = useCallback(() => {
+    editor?.commands.setContent("");
+    editor?.commands.focus();
+  }, [editor]);
 
   return (
-    <CardEditor
-      editor={editor}
-      //TODO
-      className={classes}
-      controls={
-        <RichTextEditor.Control
-          tabIndex={-1}
-          onClick={() => {
-            editor?.commands.toggleMark("gap", { group: Math.random() });
-            console.log(editor?.getHTML());
-          }}
-        >
-          <IconBracketsContain />
-        </RichTextEditor.Control>
-      }
-    />
+    <Stack gap="2rem">
+      <CardEditor
+        editor={editor}
+        className={classes}
+        controls={
+          //not used right now, use ui control or remove later
+          <RichTextEditor.Control
+            tabIndex={-1}
+            onClick={() => {
+              editor?.commands.toggleMark("gap", { group: Math.random() });
+              console.log(editor?.getHTML());
+            }}
+          >
+            <IconBracketsContain />
+          </RichTextEditor.Control>
+        }
+      />
+      <CardEditorFooter
+        finish={() => finish(mode, clear, deck, card, editor)}
+        mode={mode}
+      />
+    </Stack>
   );
+}
+
+function finish(
+  mode: EditMode,
+  clear: Function,
+  deck: Deck,
+  card: Card<CardType.Cloze> | null,
+  editor: Editor | null
+) {
+  if (mode === "edit") {
+    //ISSUE newly introduced cards through edit are not recognized
+    try {
+      if (card) {
+        ClozeCardUtils.update(
+          {
+            text: editor?.getHTML() ?? "",
+          },
+          card
+        );
+      }
+      successfullySaved();
+    } catch (error) {
+      saveFailed();
+    }
+  } else {
+    const occlusionNumberSet: number[] = getOcclusionNumberSet(
+      editor?.getHTML() ?? ""
+    );
+    try {
+      createClozeCardSet({
+        text: editor?.getHTML() ?? "",
+        occlusionNumberSet,
+      }).then((cards) => newCards(cards, deck));
+      clear();
+      successfullyAdded();
+    } catch (error) {
+      addFailed();
+    }
+  }
+}
+
+function getOcclusionNumberSet(text: string) {
+  const regex = /\{\{c\d::((?!\{\{|}}).)*\}\}/g;
+  const matches = text.match(regex);
+  const cardDigits = new Set<number>();
+  matches?.forEach((match) => {
+    const digit = parseInt(match[3]);
+    cardDigits.add(digit);
+  });
+  return Array.from(cardDigits);
 }
