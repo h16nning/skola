93fcd0b12a377da59b4a85823805c797498c0aba
h16nning <henning.flath@t-online.de>
2024-02-12 12:58:23 +0100

    chore: improve header bar options menu consistency

--- src/components/editcard/EditorOptionsMenu.tsx
+++ src/components/editcard/EditorOptionsMenu.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Menu } from "@mantine/core";
+import { ActionIcon, Menu } from "@mantine/core";
 import { IconDots, IconSettings } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 
@@ -10,7 +10,9 @@ export default function EditorOptionsMenu({}: EditorOptionsMenuProps) {
   return (
     <Menu>
       <Menu.Target>
-        <IconDots />
+        <ActionIcon variant="subtle" color="gray">
+          <IconDots />
+        </ActionIcon>
       </Menu.Target>
       <Menu.Dropdown>
         <Menu.Item
