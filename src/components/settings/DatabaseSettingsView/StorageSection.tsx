f4c0dfd1c91901969d39e6739f5b4f12bbfb6247
h16nning <henning.flath@t-online.de>
2024-02-13 13:23:50 +0100

    feat: improved storage estimate

---
+++ src/components/settings/DatabaseSettingsView/StorageSection.tsx
@@ -0,0 +1,66 @@
+import { Text, Paper, RingProgress, Group } from "@mantine/core";
+import { t } from "i18next";
+import { useEffect, useState } from "react";
+
+type StorageInfo = StorageEstimate & {
+  usageString?: string;
+  quotaString?: string;
+};
+
+function formatBytes(bytes: number, decimals = 2) {
+  if (!+bytes) return "0 Bytes";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB"];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / k ** i).toFixed(dm))} ${sizes[i]}`;
+}
+
+async function calulateStorageInfo(setStorageInfo: Function) {
+  if (navigator.storage && navigator.storage.estimate) {
+    const estimation = await navigator.storage.estimate();
+    if (estimation.usage === undefined || estimation.quota === undefined)
+      return;
+    setStorageInfo({
+      usage: estimation.usage,
+      quota: estimation.quota,
+      usageString: formatBytes(estimation.usage),
+      quotaString: formatBytes(estimation.quota),
+    });
+  }
+}
+
+export default function StorageSection() {
+  const [storageInfo, setStorageInfo] = useState<StorageInfo>({});
+
+  useEffect(() => {
+    calulateStorageInfo(setStorageInfo);
+  }, []);
+
+  return (
+    <Paper w="100%">
+      {storageInfo.usage !== undefined && storageInfo.quota !== undefined && (
+        <Group justify="space-between">
+          <RingProgress
+            size={80}
+            sections={[
+              {
+                value: (storageInfo.usage / storageInfo.quota) * 100,
+                color: "forest",
+              },
+            ]}
+            thickness={8}
+            roundCaps
+          />
+          <Text fw={600}>
+            {storageInfo.usageString} / {storageInfo.quotaString}{" "}
+            {t("settings.database.storage-used")}
+          </Text>
+        </Group>
+      )}
+    </Paper>
+  );
+}
