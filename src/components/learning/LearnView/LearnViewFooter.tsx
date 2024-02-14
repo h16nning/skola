077a84ca5605199b22cdcf8010b1418ea6d8a920
h16nning <henning.flath@t-online.de>
2024-02-14 13:42:18 +0100

    feat: local relative time estimate in LearnViewFooter

--- src/components/learning/LearnView/LearnViewFooter.tsx
+++ src/components/learning/LearnView/LearnViewFooter.tsx
@@ -1,14 +1,44 @@
 import { Button, Group } from "@mantine/core";
 import { useHotkeys } from "@mantine/hooks";
-import AnswerCardButton from "./AnswerCardButton";
-import { LearnController } from "../../../logic/learn";
-import classes from "./LearnView.module.css";
 import { Rating } from "fsrs.js";
+import i18n from "../../../i18n";
+import { LearnController } from "../../../logic/learn";
+import AnswerCardButton from "./AnswerCardButton";
+import classes from "./LearnView.module.css";
 
+const DAY_IN_MILLISECONDS = 1000 * 60 * 60 * 24;
+const MINUTE_IN_MILLISECONDS = 1000 * 60;
 interface LearnViewFooterProps {
   controller: LearnController;
   answer: Function;
 }
+
+function timeStringForRating(rating: Rating, controller: LearnController) {
+  if (!controller.currentCardRepeatInfo) {
+    return "";
+  }
+  const rtf = new Intl.RelativeTimeFormat(i18n.language, {
+    style: "short",
+    numeric: "auto",
+  });
+
+  const timeDifference =
+    controller.currentCardRepeatInfo[rating]?.card.due.getTime() - Date.now();
+  if (timeDifference >= DAY_IN_MILLISECONDS * 30) {
+    return rtf.format(
+      Math.round(timeDifference / (DAY_IN_MILLISECONDS * 30)),
+      "month"
+    );
+  } else if (timeDifference >= DAY_IN_MILLISECONDS * 0.9) {
+    return rtf.format(Math.round(timeDifference / DAY_IN_MILLISECONDS), "day");
+  } else {
+    return (
+      "~ " +
+      rtf.format(Math.round(timeDifference / MINUTE_IN_MILLISECONDS), "minute")
+    );
+  }
+}
+
 function LearnViewFooter({ controller, answer }: LearnViewFooterProps) {
   useHotkeys(
     !controller.isFinished
@@ -41,50 +71,26 @@ function LearnViewFooter({ controller, answer }: LearnViewFooterProps) {
         <Group gap="xs" wrap="nowrap" justify="center" w="100%" maw="25rem">
           <AnswerCardButton
             label="Again"
-            timeInfo={
-              (controller.currentCardRepeatInfo &&
-                controller.currentCardRepeatInfo[
-                  Rating.Again
-                ]?.card.due.toLocaleDateString()) ??
-              ""
-            }
+            timeInfo={timeStringForRating(Rating.Again, controller)}
             color="red"
             action={() => answer(Rating.Again)}
           />
           <AnswerCardButton
             label="Hard"
-            timeInfo={
-              (controller.currentCardRepeatInfo &&
-                controller.currentCardRepeatInfo[
-                  Rating.Hard
-                ]?.card.due.toLocaleDateString()) ??
-              ""
-            }
+            timeInfo={timeStringForRating(Rating.Hard, controller)}
             color="yellow"
             action={() => answer(Rating.Hard)}
           />
           <AnswerCardButton
             label="Good"
-            timeInfo={
-              (controller.currentCardRepeatInfo &&
-                controller.currentCardRepeatInfo[
-                  Rating.Good
-                ]?.card.due.toLocaleDateString()) ??
-              ""
-            }
+            timeInfo={timeStringForRating(Rating.Good, controller)}
             color="green"
             action={() => answer(Rating.Good)}
           />
           <AnswerCardButton
             label="Easy"
-            timeInfo={
-              (controller.currentCardRepeatInfo &&
-                controller.currentCardRepeatInfo[
-                  Rating.Easy
-                ]?.card.due.toLocaleDateString()) ??
-              ""
-            }
-            color="cyan"
+            timeInfo={timeStringForRating(Rating.Easy, controller)}
+            color="blue"
             action={() => answer(Rating.Easy)}
           />
         </Group>
