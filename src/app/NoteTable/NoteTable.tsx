949d3a7d0373a5b0b60cb5e3c8f147ea3c2e8c60
h16nning <henning.flath@t-online.de>
2025-03-12 18:33:05 +0100

    feat: replace data  table with mantine data table
    not feature complete

--- src/app/NoteTable/NoteTable.tsx
+++ src/app/NoteTable/NoteTable.tsx
@@ -1,125 +1,103 @@
-import { NoteType } from "@/logic/note/note";
+import { Note, NoteType } from "@/logic/note/note";
 import { NoteSortFunction, NoteSorts } from "@/logic/note/sort";
-import { Table } from "@mantine/core";
-import { useEventListener } from "@mantine/hooks";
-import { IconCards } from "@tabler/icons-react";
-import { t } from "i18next";
-import { useEffect } from "react";
-import EmptyNotice from "../../components/EmptyNotice";
-import { Note } from "../../logic/note/note";
+import clsx from "clsx";
+import { DataTable, DataTableSortStatus } from "mantine-datatable";
+import { useEffect, useState } from "react";
 import classes from "./NoteTable.module.css";
-import NoteTableHeadItem from "./NoteTableHeadItem";
-import { NoteTableItem } from "./NoteTableItem";
+import { useMediaQuery } from "@mantine/hooks";
 
 interface CardTableProps {
   noteSet: Note<NoteType>[];
-  selectedIndex: number;
-  setSelectedIndex: (index: number) => void;
-  selectedNote: Note<NoteType> | undefined;
-  setSelectedNote: (card: Note<NoteType>) => void;
   sort: [NoteSortFunction, boolean];
   setSort: (sort: [NoteSortFunction, boolean]) => void;
+  openedNote: Note<NoteType> | undefined;
+  setOpenedNote: (note: Note<NoteType> | undefined) => void;
+  openModal: () => void;
 }
 
 function NoteTable({
   noteSet,
-  selectedIndex,
-  setSelectedIndex,
-  selectedNote,
-  setSelectedNote,
-  sort,
+  openedNote,
+  setOpenedNote,
   setSort,
+  openModal,
 }: CardTableProps) {
-  const ref = useEventListener("keydown", (event) => {
-    if (event.key === "ArrowDown") {
-      setSelectedIndex(
-        selectedIndex !== undefined
-          ? Math.min(selectedIndex + 1, noteSet.length - 1)
-          : 0
-      );
-    } else if (event.key === "ArrowUp") {
-      setSelectedIndex(
-        selectedIndex !== undefined
-          ? Math.max(selectedIndex - 1, 0)
-          : noteSet.length - 1
-      );
-    }
+  const [selectedNotes, setSelectedNotes] = useState<Note<NoteType>[]>([]);
+  
+  const [sortStatus, setSortStatus] = useState<DataTableSortStatus<Note<NoteType>>>({
+    columnAccessor: 'sortField',
+    sortKey: 'bySortField',
+    direction: 'asc',
   });
 
-  //Set the selected index to 0 if there is no selected index (i.e. on page load)
   useEffect(() => {
-    if (selectedIndex === -1) {
-      setSelectedIndex(0);
-    }
-  }, [selectedIndex]);
+    setSort([NoteSorts[sortStatus.sortKey as keyof typeof NoteSorts], sortStatus.direction === 'asc']);
+  }, [sortStatus]);
+
+
+  const isTouch = useMediaQuery('(pointer: coarse)');
+  const isMobile = useMediaQuery('(max-width: 50em)');
 
   return (
-    <Table.ScrollContainer
-      minWidth="500px"
-      type="native"
-      className={classes.tableScrollContainer}
-    >
-      <Table
-        className={classes.table}
-        highlightOnHover
-        withRowBorders={false}
-        withColumnBorders={false}
-        striped
-        stickyHeader
-        ref={ref}
-        tabIndex={0}
-      >
-        <Table.Thead>
-          <Table.Tr className={classes.tr}>
-            <NoteTableHeadItem
-              name={"Name"}
-              sortFunction={NoteSorts.bySortField}
-              sort={sort}
-              setSort={setSort}
-            />
-            <NoteTableHeadItem
-              name={"Type"}
-              sortFunction={NoteSorts.byType}
-              sort={sort}
-              setSort={setSort}
-            />
-            <NoteTableHeadItem
-              name={"Deck"}
-              sortFunction={NoteSorts.byDeckName}
-              sort={sort}
-              setSort={setSort}
-            />
-            <NoteTableHeadItem
-              name={"Creation Date"}
-              sortFunction={NoteSorts.byCreationDate}
-              sort={sort}
-              setSort={setSort}
-            />
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {noteSet.map((note, index) => (
-            <NoteTableItem
-              note={note}
-              key={note.id}
-              index={index}
-              selectedIndex={selectedIndex}
-              setSelectedIndex={setSelectedIndex}
-              selectedNote={selectedNote}
-              setSelectedNote={setSelectedNote}
-            />
-          ))}
-        </Table.Tbody>
-      </Table>
-      {noteSet.length === 0 && (
-        <EmptyNotice
-          icon={IconCards}
-          description={t("manage-cards.table.no-cards-found")}
-          hideTitle
-          p="xl"
-        />
-      )}
-    </Table.ScrollContainer>
+    <DataTable
+      className={classes.table}
+      records={noteSet}
+      columns={[
+        {
+          accessor: "sortField",
+          title: "Name",
+          ellipsis: true,
+          width: 200,
+          resizable: true,
+          filtering: true,
+          sortable: true,
+          sortKey: "bySortField",
+        },
+        {
+          accessor: "creationDate",
+          title: "Creation Date",
+          render: (note) => note.creationDate.toLocaleDateString(),
+          resizable: true,
+          sortable: true,
+          sortKey: "byCreationDate",
+        },
+        {
+          accessor: "content.type",
+          title: "Note Type",
+          resizable: true,
+          sortable: true,
+          sortKey: "byType",
+        },
+      ]}
+      withTableBorder={false}
+      withRowBorders={false}
+      highlightOnHover
+      borderRadius="md"
+      striped="odd"
+      height="100%"
+      textSelectionDisabled={isTouch}
+      selectionCheckboxProps={{ size: isMobile ? "sm" : "xs" }}
+      selectedRecords={selectedNotes}
+      onSelectedRecordsChange={setSelectedNotes}
+      sortStatus={sortStatus}
+      onSortStatusChange={setSortStatus}
+      rowClassName={(record) =>
+        clsx({
+          [classes.selected]: record.id === openedNote?.id,
+          [classes.row]: true,
+        })
+      }
+      onRowClick={(row) => {
+        setOpenedNote(row.record);
+        if(isMobile) {
+          openModal();
+        }
+      }}
+      onRowDoubleClick={(row) => {
+        setOpenedNote(row.record);
+        openModal();
+      }}
+    />
   );
 }
 
