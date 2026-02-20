6208584c9ad55aa18747b18b3090f9a42e128447
Henning Flath <henning.flath@t-online.de>
2026-02-20 21:05:24 +0100

    chore: temporarliy remove broken Swipe Layout

--- src/app/learn/LearnView/LearnView.tsx
+++ src/app/learn/LearnView/LearnView.tsx
@@ -7,8 +7,7 @@ import { getAdapter } from "@/logic/NoteTypeAdapter";
 import { useDeckFromUrl } from "@/logic/deck/hooks/useDeckFromUrl";
 import { useNote } from "@/logic/note/hooks/useNote";
 import { useSetting } from "@/logic/settings/hooks/useSetting";
-import { Rating } from "fsrs.js";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import FinishedLearningView from "../FinishedLearningView/FinishedLearningView";
 import LearnViewCurrentCardStateIndicator from "../LearnViewCurrentCardStateIndicator/LearnViewCurrentCardStateIndicator";
@@ -17,8 +16,6 @@ import "./LearnView.css";
 import LearnViewFooter from "./LearnViewFooter";
 import LearnViewHeader, { stopwatchResult } from "./LearnViewHeader";
 import QuickAddNote from "./QuickAddNote";
-import SwipeBackground from "./SwipeBackground";
-import SwipeCard from "./SwipeCard";
 import VisualFeedback from "./VisualFeedback";
 import { useLearnSession } from "./useLearnSession";
 
@@ -31,9 +28,6 @@ function LearnView() {
 
   const navigate = useNavigate();
   const [deck, isReady, params] = useDeckFromUrl();
-
-  const [swipeProgress, setSwipeProgress] = useState(0);
-  const [swipeDirection, setSwipeDirection] = useState<"left" | "right" | null>(null);
 
   const {
     controller,
@@ -60,24 +54,6 @@ function LearnView() {
       stopwatchResult && stopwatchResult.start();
     }
   }, [controller.isFinished]);
-
-  const handleSwipeProgress = (progress: number, direction: "left" | "right" | null) => {
-    setSwipeProgress(progress);
-    setSwipeDirection(direction);
-  };
-
-  const handleSwipeLeft = () => {
-    if (controller.showingAnswer) {
-      answerCard(Rating.Again);
-    }
-  };
-
-  const handleSwipeRight = () => {
-    if (controller.showingAnswer) {
-      answerCard(Rating.Good);
-    }
-  };
-
   if (isReady && !deck) {
     return <NotFound />;
   }
@@ -94,57 +70,47 @@ function LearnView() {
 
       <div className={`${BASE}__wrapper`}>
         {useVisualFeedback && <VisualFeedback rating={currentRating} />}
-        <SwipeBackground swipeProgress={swipeProgress} direction={swipeDirection}>
-          <div className={`${BASE}__card-container`}>
-            <div className={`${BASE}__card-stack`}>
-              <LearnViewCurrentCardStateIndicator
-                currentCardModel={controller.currentCard?.model}
-              />
-              <SwipeCard
-                onSwipeLeft={handleSwipeLeft}
-                onSwipeRight={handleSwipeRight}
-                onSwipeProgress={handleSwipeProgress}
-                disabled={!controller.showingAnswer}
-                className={`${BASE}__card`}
-              >
-                <Paper shadow="xs" withBorder>
-                  {!controller.showingAnswer &&
-                    controller.currentCard &&
-                    getAdapter(controller.currentCard).displayQuestion(
-                      controller.currentCard,
-                      cardContent
-                    )}
-                  {controller.showingAnswer &&
-                    controller.currentCard &&
-                    getAdapter(controller.currentCard).displayAnswer(
-                      controller.currentCard,
-                      cardContent
-                    )}
-                </Paper>
-              </SwipeCard>
-              {showCognitivePrompts && controller.showingAnswer && (
-                <>
-                  {showPrompts && (
-                    <CognitivePromptConnector
-                      onToggle={togglePrompt}
-                      selectedPrompt={selectedPrompt}
+        <div className={`${BASE}__card-container`}>
+          <div className={`${BASE}__card-stack`}>
+            <LearnViewCurrentCardStateIndicator
+              currentCardModel={controller.currentCard?.model}
+            />
+            <Paper shadow="xs" withBorder className={`${BASE}__card`}>
+              {!controller.showingAnswer &&
+                controller.currentCard &&
+                getAdapter(controller.currentCard).displayQuestion(
+                  controller.currentCard,
+                  cardContent
+                )}
+              {controller.showingAnswer &&
+                controller.currentCard &&
+                getAdapter(controller.currentCard).displayAnswer(
+                  controller.currentCard,
+                  cardContent
+                )}
+            </Paper>
+            {showCognitivePrompts && controller.showingAnswer && (
+              <>
+                {showPrompts && (
+                  <CognitivePromptConnector
+                    onToggle={togglePrompt}
+                    selectedPrompt={selectedPrompt}
+                  />
+                )}
+                {selectedPrompt &&
+                  deck &&
+                  typeof controller.currentCard?.note === "string" && (
+                    <QuickAddNote
+                      deck={deck}
+                      sourceNoteId={controller.currentCard.note}
+                      prompt={selectedPrompt}
+                      onClose={closeQuickAdd}
                     />
                   )}
-                  {selectedPrompt &&
-                    deck &&
-                    typeof controller.currentCard?.note === "string" && (
-                      <QuickAddNote
-                        deck={deck}
-                        sourceNoteId={controller.currentCard.note}
-                        prompt={selectedPrompt}
-                        onClose={closeQuickAdd}
-                      />
-                    )}
-                </>
-              )}
-            </div>
+              </>
+            )}
           </div>
-        </SwipeBackground>
+        </div>
         <LearnViewFooter controller={controller} answer={answerCard} />
 
         {debouncedFinish && (
